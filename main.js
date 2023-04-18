70ce3056e0bd86f2abc996e4656b051ab232cfb0
Stan Girard <girard.stanislas@gmail.com>
2023-04-18 12:19:20 +0200

    feat(style): moved to own file

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
